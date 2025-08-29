217afa9f3ec2a86f6cf2aad0894075658c1fd3ae
Sadekur Rahman <shadekur.rahman60@gmail.com>
2025-08-30 02:14:37 +0600

    error fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-// Replace the mockFirebase with real Firebase imports
-import { auth, db } from './firebase';
-import { 
-  signInWithEmailAndPassword, 
-  signOut, 
-  onAuthStateChanged 
-} from 'firebase/auth';
-import { 
-  doc, 
-  setDoc, 
-  onSnapshot 
-} from 'firebase/firestore';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-3xl font-bold text-red-500'>App</h1>
-    </div>
+    <h2 className='text-4xl text-red-800 text-center'>Hello</h2>
   );
 }
 
