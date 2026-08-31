88b7f518dc5a936d1f96377135894c32edd203d8
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-08-31 21:24:34 +0600

    Add site configuration with contact information and social media links

---
+++ lib/siteConfig.ts
@@ -0,0 +1,7 @@
+// Language-independent facts pulled from the foundation's brochure (As-Salsabil Foundation.pdf).
+export const siteConfig = {
+  phones: ["01780-664660", "01717-136456"],
+  email: "salsabilfoundation1@gmail.com",
+  youtube: { label: "As-Salsabil tv", href: "https://www.youtube.com/@As-Salsabiltv" },
+  facebook: { label: "As Salsabil-foundation", href: "https://www.facebook.com/AsSalsabilfoundation" },
+};
