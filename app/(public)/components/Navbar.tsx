a675643fcbac9d2a6ae57da1f0e327ffe6eb13b2
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-08-31 23:54:35 +0600

    style: import Image component from next/image in Navbar for enhanced image handling

--- app/(public)/components/Navbar.tsx
+++ app/(public)/components/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 import { Menu, X } from "lucide-react";
