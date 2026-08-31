c915b9ffec8619e93daab3191cdbbb81741d61d8
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-08-31 23:37:33 +0600

    style: update layout background color for improved aesthetics

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -5,7 +5,7 @@ import { Footer } from "./components/Footer";
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
     <LanguageProvider>
-      <div className="min-h-screen flex flex-col bg-gray-50">
+      <div className="min-h-screen flex flex-col bg-stone-50">
         <Navbar />
         <main className="flex-1">{children}</main>
         <Footer />
